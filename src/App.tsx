db4a5b6d53e17313f4dc110f0fcc5943c28635e8
YashJindal0707 <Yash.Jindal@celsiortech.com>
2025-12-23 18:01:43 +0530

    feat(AUTH-201): add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+// import DashboardPage from './pages/DashboardPage';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      {/* <Route path="/dashboard" element={<DashboardPage />} /> */}
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
